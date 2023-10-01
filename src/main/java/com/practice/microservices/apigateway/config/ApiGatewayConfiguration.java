0fc4bea196121b53d1d4aa8495a779fd7a948f1e
Kanav Ghai <kanghai@publicisgroupe.net>
2023-10-01 13:13:08 +0530

    feat: make routes simpler

--- src/main/java/com/practice/microservices/apigateway/config/ApiGatewayConfiguration.java
+++ src/main/java/com/practice/microservices/apigateway/config/ApiGatewayConfiguration.java
@@ -16,9 +16,19 @@ public class ApiGatewayConfiguration {
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
         return builder
                 .routes()
-                .route(p -> p.path("/get")
+                .route(p -> p
+                        .path("/get")
                         .filters(f -> f.addRequestHeader("x-my-header", "headerValue"))
                         .uri("http://httpbin.org:80"))
+                .route(p -> p
+                        .path("/currency-exchange/**")
+                        .uri("lb://currency-exchange-service"))
+                .route(p -> p
+                        .path("/currency-convertor/**")
+                        .uri("lb://currency-conversion-service"))
+                .route(p -> p
+                        .path("/currency-convertor-feign/**")
+                        .uri("lb://currency-conversion-service"))
                 .build();
     }
 }
